9848775122f3505cef36e5a1f2dd9daa0f82953d
alves-luis <24353152+alves-luis@users.noreply.github.com>
2018-12-06 14:04:07 +0000

    Started authentication

--- CloudAlloc/src/main/java/cloudalloc/ServerWriter.java
+++ CloudAlloc/src/main/java/cloudalloc/ServerWriter.java
@@ -9,6 +9,25 @@ package cloudalloc;
  *
  * @author Luís Alves
  */
-public class ServerWriter {
-    
+public class ServerWriter implements Runnable {
+
+    private Socket s;
+    private CloudAlloc c;
+    private PrintWriter out;
+    private boolean loggedIn;
+
+    public ServerWriter(Socket s, CloudAlloc c) {
+      this.s = s;
+      this.c = c;
+      this.out = new PrintWriter(s.getOutputStream(),true);
+      this.loggedIn = false;
+    }
+
+    public void run() {
+      login();
+    }
+
+    private void login() {
+      out.println(Menu.show(0));
+    }
 }
